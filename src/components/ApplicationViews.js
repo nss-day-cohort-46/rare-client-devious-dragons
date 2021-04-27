1e12c9df25cd8f247e55a5277c0ec8ca4efbeb60
samj0516 <samj0516@gmail.com>
2021-04-27 15:08:04 -0500

    added comment provider and comment form, New comments post to server side

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -8,7 +8,8 @@ import { PostDetail } from './posts/PostDetail'
 import { CategoryProvider } from "./categories/CategoryProvider"
 import { CategoryList } from "./categories/CategoryList"
 import { CategoryForm } from "./categories/CategoryForm"
-
+import { CommentForm } from './comments/CommentForm'
+import { CommentProvider } from './comments/CommentProvider'
 
 export const ApplicationViews = () => {
     return <>
@@ -18,30 +19,31 @@ export const ApplicationViews = () => {
         }}>
             <PostProvider>
                 <CategoryProvider>
-
-                    <Route exact path="/posts/detail/:postId(\d+)">
-                        <PostDetail />
-                    </Route>
-
-
-
-                    <Route path="/myposts">
-                        <PostList />
-                    </Route>
-
-                    <Route path="/posts">
-                        <PostList />
-                    </Route>
-
-                    <Route exact path="/categories">
-                        <CategoryList />
-                    </Route>
-
-                    <Route path="/categories/create">
-                        <CategoryForm />
-                    </Route>
+                    <CommentProvider>
+                        <Route exact path="/posts/detail/:postId(\d+)">
+                            <PostDetail />
+                            <CommentForm />
+                        </Route>
+                        
 
 
+                        <Route exact path="/myposts">
+                            <PostList />
+                        </Route>
+
+                        <Route exact path="/posts">
+                            <PostList />
+                        </Route>
+
+                        <Route exact path="/categories">
+                            <CategoryList />
+                        </Route>
+
+                        <Route path="/categories/create">
+                            <CategoryForm />
+                        </Route>
+
+                    </CommentProvider>
                 </CategoryProvider>
 
             </PostProvider>
