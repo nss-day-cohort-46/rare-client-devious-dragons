4f5735187d2a4c6e56026096ec18879433e9fce4
sllittrell <stacey.littrell@gmail.com>
2021-04-27 14:03:21 -0500

    tag form created and tags populate tag page

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -17,7 +17,7 @@ export const TagList= () => {
             <button className="add_tag" onClick={() => history.push("tags/create")}>Create Tag</button>
             <div>
                 <h3>Created Tags</h3>
-                <div className="tag_list">{tags.map(t => t.label)}</div>
+                <div className="tag_list">{tags.map(t => <div>{t.label}</div>)}</div>
             </div>
         </>
     )
