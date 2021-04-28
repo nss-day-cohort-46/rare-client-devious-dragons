12888ac7acd7920ae42be2f99fb46ef272315d0c
Alex Martin <alex@subtleco.co>
2021-04-28 11:15:09 -0500

    added comments about sorting

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -12,6 +12,7 @@ export const UserList = () => {
 
     useEffect(() => {
         // Using the array.sort method to put these in alphabetical order
+        // if interested in how this works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
         setSortedUsers(
             users.sort( (a,b) => {
                 if (a.userName.toUpperCase() < b.userName.toUpperCase()) return -1
