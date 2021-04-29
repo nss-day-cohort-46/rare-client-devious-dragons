6e7ca5870928f1968fc7c93a8285d5dd921bf3d8
Alex Martin <alex@subtleco.co>
2021-04-29 12:35:23 -0500

    User can now subscribe and unsubscribe, barring themselves

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router'
+import { SubscriptionContext } from '../subscriptions/SubscriptionProvider'
 import './UserCard.css'
 
-export const UserCard = ({ user }) => {
+export const UserCard = ({ user, subscribed, subscription, isSelf }) => {
     const history = useHistory()
+    const { addSubscription, deleteSubscription } = useContext(SubscriptionContext)
+    const [cardSubscription, setCardSubscription] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+    const userId = parseInt(localStorage.getItem(`rare_user_id`))
+
+    const handleSubscribe = e => {
+        setIsLoading(true)
+        const authorId = parseInt(e.target.id.split("--")[1])
+        const subscription = {
+            followerId: userId,
+            authorId: authorId
+        }
+        addSubscription(subscription)
+            .then(setCardSubscription)
+    }
+
+    const handleUnsubscribe = e => {
+        setIsLoading(true)
+        deleteSubscription(cardSubscription.id)
+            .then(() => {
+                setCardSubscription({})
+            })
+    }
+
+    useEffect(() => {
+        setCardSubscription(subscribed)
+    }, [])
+
+    useEffect(() => {
+        setIsLoading(false)
+    }, [cardSubscription])
 
 
 
@@ -12,6 +44,16 @@ export const UserCard = ({ user }) => {
             <h2 className="userNameLink" onClick={() => history.push(`/users/${user.id}`)}>{user.userName}</h2>
             <h3>{user.firstName} {user.lastName}</h3>
             <p>Everyone is an admin!</p>
+            {/* ternary - if the user card belongs to the current user, return "this is you", if not, show buttons */}
+            {/* ternary - if not subscrubed, show subscribe button, else show unsub button */}
+            {isSelf ?
+                <p className="thisIsYou">this is you!</p>
+                :
+                Object.keys(cardSubscription).length ?
+                    <button onClick={handleUnsubscribe} disabled={isLoading} className="unsubscribeButton" id={"unsubscribeButton--" + user.id}>Unsubscibe</button>
+                    :
+                    <button onClick={handleSubscribe} disabled={isLoading} className="subscribeButton" id={"subscribeButton--" + user.id}>Subscribe</button>
+            }
         </article>
     )
 }
--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,12 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { SubscriptionContext } from '../subscriptions/SubscriptionProvider'
 import { UserCard } from './UserCard'
 import { UserContext } from './UserProvider'
 
 export const UserList = () => {
     const { users, getAllUsers } = useContext(UserContext)
+    const { subscriptions, getSubscriptions } = useContext(SubscriptionContext)
     const [sortedUsers, setSortedUsers] = useState([])
+    const userId = parseInt(localStorage.getItem(`rare_user_id`))
 
     useEffect(() => {
+        getSubscriptions()
         getAllUsers()
     }, [])
 
@@ -27,7 +31,22 @@ export const UserList = () => {
             <h1> Look at <a href="https://www.youtube.com/watch?v=NsLKQTh-Bqo">allllll those</a> Users</h1>
             {
                 sortedUsers.map(user => {
-                    return <UserCard key={user.id} user={user} />
+                    let isSelf = (user.id === userId)
+                    
+                    let subscribed
+                    // filter for user's subscriptions
+                    const userSubs = subscriptions.filter(sub => sub.followerId === userId)
+                    
+                    // Is the current user subscribed to THIS user?
+                    let subscription = userSubs.find(sub => sub.authorId === user.id)
+                    debugger
+                    if (subscription || userId === user.id) {
+                        subscribed = true
+                    } else {
+                        subscribed = false
+                    }
+                    console.log(subscribed)
+                    return <UserCard key={user.id} user={user} subscribed={subscribed} subscription={subscription} isSelf={isSelf}/>
                 })
             }
         </section>
