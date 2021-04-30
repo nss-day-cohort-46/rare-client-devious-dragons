e2599a71f9d87e73e9b2aaa869c4f1a56d2ef920
sllittrell <stacey.littrell@gmail.com>
2021-04-30 13:40:43 -0500

    fixed

--- src/components/reactions/ReactionProvider.js
+++ src/components/reactions/ReactionProvider.js
@@ -42,16 +42,16 @@ export const ReactionProvider = props => {
                 
     }
 
-    const getReactionsByReactionId = (reactionId) =>{
-        return fetch(`http://localhost:8088/postReaction?reaction=${rectionId}`)
-        .then(res => res.json())
-        .then(setPostComments)
-    }
+    // const getReactionsByReactionId = (reactionId) =>{
+    //     return fetch(`http://localhost:8088/postReaction?reaction=${rectionId}`)
+    //     .then(res => res.json())
+    //     .then(setPostComments)
+    // }
 
 
     return (
         <ReactionContext.Provider value={{
-            reactions, getReactions, addReaction, getPostReactions, postReact, addPostReaction, getReactionByReactionId
+            reactions, getReactions, addReaction, getPostReactions, postReact, addPostReaction
         }}>
             {props.children}
         </ReactionContext.Provider>
